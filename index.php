9822e69a9703ed91921c3e4cf0fd63fc494cff9a
fabian1999bat <fabian1999bat@gmail.com>
2018-04-03 22:01:48 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE HTML>
+<?php
+if(isset($_POST['submit'])){
+    $to = "email@example.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+
+    mail('A01630024@itesm.mx','recoleccion de correos',$_POST['email'],$_POST['message']);
+
+
+    }
+?>
+
+<html lang="en">
+  <head>
+      <meta charset="utf-8">
+      <html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          <meta name="description" content="">
+          <meta name="author" content="">
+          <link rel="icon" href="images/favicon.png">
+          <link href="css/print.css" rel="stylesheet" media="print" type="text/css" />
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.10/semantic.min.css">
+          <script src="https://cdn.jsdelivr.net/semantic-ui/2.2.10/semantic.min.js"></script>
+           <script src="js/print.js"></script>
+
+          <title>Altimedic - Altitud en Medicamentos</title>
+
+          <!-- Bootstrap core CSS -->
+          <link href="./Pricing example for Bootstrap_files/bootstrap.min.css" rel="stylesheet">
+
+          <!-- Custom styles for this template -->
+          <link href="./Pricing example for Bootstrap_files/pricing.css" rel="stylesheet">
+
+        <style></style></head>
+  </head>
+  <body class="text-center">
+    <div class="cover-container">
+      <header class="masthead mb-auto">
+          <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-bottom box-shadow">
+            <h1 class="my-0 mr-md-auto font-weight-normal">ALTIMEDIC</h1>
+
+            <nav class="my-2 my-md-0 mr-md-3">
+              <a class="p-2 text-dark" href="index.php">Inicio</a>
+              <a class="p-2 text-dark" href="test.html">¿Quienes sómos?</a>
+              <a class="p-2 text-dark" href="Offert.html">Catálogo</a>
+
+
+                  <button onclick="myFunction()" class="ui icon button" title="print">
+                    <i class="print icon"></i>
+                  </button>
+
+
+
+
+
+            </nav>
+      </div>
+
+      </header>
+      <img class="ui image" src="images/background.jpg" alt="fondo">
+      <br></br>
+      <br></br>
+      <div class="row">
+      <div class="col-lg-7">
+
+      <main role="main" class="container">
+        <h1 class="cover-heading">Altitud en Medicamentos</h1>
+        <br></br>
+        <p class="lead">Bienvenido a Altimedic, una empresa local distribuidora de medicamentos y productos de: Sanfer, Hormona, Medley, Kendrick, Valeant, entre muchas más. Aquí podras encontraras un amplio cataogo de medicamentos de alta cálidad con fines comerciales o personales con precios bajos.</p>
+         <br></br>
+        </div>
+
+
+
+<div class="col-lg-5">
+    <div class="thumbnail center well well-small">
+        <h2>¡SUSCRÍBETE A ALTIMEDICcc!</h2>
+
+        <p>Suscríbete para recibir las mejores noticias y ofertas de Altimedic</p>
+
+
+
+
+
+        <form action="" method="post">
+        Nombre: <input type="text" name="first_name" title="nombre"><br>
+        Apellidos: <input type="text" name="last_name" title="apellidos"><br>
+        Email: <input type="text" name="email" title="email"><br>
+        Sugerencias o dudas:<br><textarea rows="5" name="message" cols="30" title="sugerencias"></textarea><br>
+        <input type="submit" name="submit" value="Submit">
+        </form>
+
+    </div>
+</div>
+</div>
+
+        <br></br>
+        <br></br>
+
+
+        <a href="test.html" id="boton"
+        <button class="ui right labeled icon button">
+          <i class="right arrow icon"></i>
+          Conócenos
+        </button>
+        </a>
+
+
+
+
+
+
+    <div class="span12">
+
+    </div>
+
+
+
+
+      </main>
+      <br></br>
+      <br></br>
+
+      <footer class="pt-4 my-md-5 pt-md-5 border-top">
+          <footer class="container">
+            <p>&copy; 2017-2018 Altimedic, Inc. &middot; <a href="test.html"> <font color="#0072ed">Sobre nosotros</font></a> &middot; Correo: altimedic01@gmail.com </p>
+            <p> Contacto: Francisco Ramírez Rojas </p>
+            <a href=" https://www.facebook.com/altimedicfrancisco/" id="Facebook" title="Facebook"
+            <button class="ui circular facebook icon button">
+              <i class="facebook icon"></i>
+            </button>
+            </a>
+            <a href="https://twitter.com/francisco0174" id="Twitter" title="Twitter"
+            <button class="ui circular twitter icon button">
+              <i class="twitter icon"></i>
+            </button>
+            </a>
+
+            <button class="ui red button" >+52 3338097927</button>
+          </footer>
+
+
+
+
+          <br></br>
+
+      </footer>
+
+
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./Pricing example for Bootstrap_files/jquery-3.2.1.slim.min.js.descarga" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="./Pricing example for Bootstrap_files/popper.min.js.descarga"></script>
+    <script src="./Pricing example for Bootstrap_files/bootstrap.min.js.descarga"></script>
+    <script src="./Pricing example for Bootstrap_files/holder.min.js.descarga"></script>
+    <script>
+      Holder.addTheme('thumb', {
+        bg: '#55595c',
+        fg: '#eceeef',
+        text: 'Thumbnail'
+      });
+    </script>
+  </body>
+</html>
